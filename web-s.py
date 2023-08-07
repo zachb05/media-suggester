aaaa1cbfafb0ce80c99332d93f5407656118fcab
Zachary Barandino <zachary.barandino@gmail.com>
2023-08-07 12:09:26 -0700

    Use bs4 to scrape MAL and write to a .csv

--- web-s.py
+++ web-s.py
@@ -1,6 +1,21 @@
-from selenium import webdriver
-from BeautifulSoup import BeautifulSoup
-import pandas as pd
+from bs4 import BeautifulSoup
+import csv
+import requests
 
-driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
+page_to_scrape = requests.get("https://myanimelist.net/anime/season")
+soup = BeautifulSoup(page_to_scrape.text, "html.parser")
 
+titles = soup.findAll("a", href = True, attrs={"class":"link-title"})
+ratings = soup.findAll("div", attrs={"title":"Score"})
+
+file = open("scraped_animes.csv", "w")
+writer = csv.writer(file)
+
+writer.writerow(["Titles", "Ratings"])
+                 
+for title, rating in zip(titles, ratings):
+    if "N/A" in rating.text:
+        continue
+    if float(rating.text) >= 8:
+        writer.writerow([title.text, rating.text])
+file.close()
