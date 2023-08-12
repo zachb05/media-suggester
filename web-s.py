33d4d8ff8151b1fa5946605b753aae1c56878318
Zachary Barandino <zachary.barandino@gmail.com>
2023-08-11 20:10:49 -0700

    Limit csv entries to 10

--- web-s.py
+++ web-s.py
@@ -11,10 +11,6 @@ def contains_keyword(keywords, review):
 def get_review_text(review):
     return review.find("div", attrs={"class":"text"}).text
 
-page_to_scrape = requests.get("https://myanimelist.net/reviews.php")
-soup = BeautifulSoup(page_to_scrape.text, "html.parser")
-
-review_list = soup.findAll("div", attrs={"class":"review-element"})
 titles = list()
 reviews = list()
 ratings = list()
@@ -22,23 +18,34 @@ links = list()
 
 keywords = input("What keyword do you want to search for? (For multiple terms, separate with a comma and space) ").split(", ")
     
-for review in review_list:
-    if not contains_keyword(keywords,review):
-        continue
-    
-    raw_review = get_review_text(review)
-    reviews.append(raw_review.strip().replace("...", "").replace("\n", "").replace("\r", "").replace("\t", ""))
-    
-    title = review.find("a", attrs={"class":"title"})
-    titles.append(title.text)
-    
-    link = title['href']
-    links.append(link)
-    
-    page_to_scrape = requests.get(link)
+iteration = 0
+while len(reviews) < 10:
+    iteration += 1
+    print(iteration)
+    page_to_scrape = requests.get("https://myanimelist.net/reviews.php&p={iteration}")
     soup = BeautifulSoup(page_to_scrape.text, "html.parser")
-    rating = soup.find("div", attrs={"class":"score-label"}).text
-    ratings.append(rating)
+    review_list = soup.findAll("div", attrs={"class":"review-element"})
+    
+    for review in review_list:
+        if not contains_keyword(keywords,review):
+            continue
+        
+        raw_review = get_review_text(review)
+        reviews.append(raw_review.strip().replace("...", "").replace("\n", "").replace("\r", "").replace("\t", ""))
+        
+        title = review.find("a", attrs={"class":"title"})
+        titles.append(title.text)
+        
+        link = title['href']
+        links.append(link)
+        
+        page_to_scrape = requests.get(link)
+        soup = BeautifulSoup(page_to_scrape.text, "html.parser")
+        rating = soup.find("div", attrs={"class":"score-label"}).text
+        ratings.append(rating)
+        
+        if len(reviews) >= 10:
+            break
     
 file = open("suggested_media.csv", "w")
 writer = csv.writer(file)
