9841bedbfdb4bcc37a4c478f7def14a9dc004ee3
Zachary Barandino <zachary.barandino@gmail.com>
2023-08-15 10:58:59 -0700

    Fix duplicate entry bug

--- web-s.py
+++ web-s.py
@@ -2,15 +2,25 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 
+# Checks if keywords are in the text retrieved by a single page of reviews
 def contains_keyword(keywords, review):
     for keyword in keywords:
         if not keyword.lower() in get_review_text(review).lower():
             return False
     return True
 
+# Returns the review text within the soup instance 'review'
 def get_review_text(review):
     return review.find("div", attrs={"class":"text"}).text
 
+# Checks if review has already been tracked
+def is_same_review(title, titles):
+    for text in titles:
+        if text == title:
+            return True
+    return False
+
+# Lists to hold .csv entries
 titles = list()
 reviews = list()
 ratings = list()
@@ -21,20 +31,25 @@ keywords = input("What keyword do you want to search for? (For multiple terms, s
 iteration = 0
 while len(reviews) < 10:
     iteration += 1
-    print(iteration)
+    print("Searching page " + str(iteration) + "...")
+    
+    # Sifts through MAL's 'New reviews' page
     page_to_scrape = requests.get("https://myanimelist.net/reviews.php&p={iteration}")
     soup = BeautifulSoup(page_to_scrape.text, "html.parser")
     review_list = soup.findAll("div", attrs={"class":"review-element"})
     
+    # Appends information to above lists iff keywords are contained in review text
     for review in review_list:
         if not contains_keyword(keywords,review):
             continue
         
+        title = review.find("a", attrs={"class":"title"})
+        if is_same_review(title.text, titles):
+            continue
+        titles.append(title.text)
+        
         raw_review = get_review_text(review)
         reviews.append(raw_review.strip().replace("...", "").replace("\n", "").replace("\r", "").replace("\t", ""))
-        
-        title = review.find("a", attrs={"class":"title"})
-        titles.append(title.text)
         
         link = title['href']
         links.append(link)
@@ -44,14 +59,22 @@ while len(reviews) < 10:
         rating = soup.find("div", attrs={"class":"score-label"}).text
         ratings.append(rating)
         
+        # Ceases search after 10 valid reviews have been found
         if len(reviews) >= 10:
             break
     
-file = open("suggested_media.csv", "w")
-writer = csv.writer(file)
+    # Ceases search after 50 pages (250 reviews)
+    if iteration >= 50:
+        print("Not enough reviews (" + str(len(titles)) + ") found with keywords " + str(keywords[:]))
+        break
+    
+# Writes to a .csv file if at least 1 review was found
+if len(titles) > 0:
+    file = open("suggested_media.csv", "w")
+    writer = csv.writer(file)
 
-writer.writerow(["Titles", "Reviews", "Ratings", "Links"])
-                 
-for title, review, rating, link in zip(titles, reviews, ratings, links):
-    writer.writerow([title, review, rating, link])
-file.close()
+    writer.writerow(["Titles", "Reviews", "Ratings", "Links"])
+                    
+    for title, review, rating, link in zip(titles, reviews, ratings, links):
+        writer.writerow([title, review, rating, link])
+    file.close()
